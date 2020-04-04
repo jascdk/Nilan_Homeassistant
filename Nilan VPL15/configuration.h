262521c0568b2eb90510afa223006932461e9dfc
jascdk <35434108+jascdk@users.noreply.github.com>
2020-04-04 23:28:57 +0200

    Update configuration.h

--- Nilan VPL15/configuration.h
+++ Nilan VPL15/configuration.h
@@ -7,8 +7,8 @@
 #define CONFIGURED 1
 
 // WIFI settings
-#define WIFISSID     "Fam.Scherrebeck.Net" // Put in your SSID
-#define WIFIPASSWORD "Jacob1905" // Put in you SSID Password
+#define WIFISSID     "XXXXX" // Put in your SSID
+#define WIFIPASSWORD "XXXXXXXX" // Put in you SSID Password
 #define CUSTOM_HOSTNAME "Nilan_MQTT"  // Hostname of the ESP8266 so that it's easier to find in your DHCP range
 
 // LED settings
@@ -16,9 +16,9 @@
 #define USE_WIFI_LED false // if 'true', the blue led in a NodeMCU will blink during connection,
                            // and glow solid once connected
 // MQTT settings
-#define MQTTSERVER   "10.0.1.2" // Put in the IP adresse of your MQTT broker
-#define MQTTUSERNAME "JacobJS" // Username for the MQTT broker (NULL if no username is required)
-#define MQTTPASSWORD "Jacob1905Jacob" // Passowrd for the MQTT broker (NULL if no password is required)
+#define MQTTSERVER   "XXXXXXX // Put in the IP adresse of your MQTT broker
+#define MQTTUSERNAME "XXXXX" // Username for the MQTT broker (NULL if no username is required)
+#define MQTTPASSWORD "XXXXXXXX" // Passowrd for the MQTT broker (NULL if no password is required)
 
 // Serial port
 #define SERIAL       SERIAL_HARDWARE // SERIAL_SOFTWARE or SERIAL_HARDWARE
