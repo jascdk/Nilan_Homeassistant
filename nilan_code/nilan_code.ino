24be4b97855e82d0ee4430c45652823e66e8162a
jascdk <35434108+jascdk@users.noreply.github.com>
2020-05-19 22:23:53 +0200

    relay update

--- nilan_code/nilan_code.ino
+++ nilan_code/nilan_code.ino
@@ -1,3 +1,26 @@
+/**
+  Nilan Modbus firmware for D1 Mini (ESP8266) together with a TTL to RS485 Converter https://www.aliexpress.com/item/32836213346.html?spm=a2g0s.9042311.0.0.27424c4dqnr5i7
+ 
+  Written by Dan Gunvald
+    https://github.com/DanGunvald/NilanModbus
+  Modified to use with Home Assistant by Anders Kvist, Jacob Scherrebeck and other great people :)
+    https://github.com/anderskvist/Nilan_Homeassistant
+    https://github.com/jascdk/Nilan_Homeassistant
+   
+  Read from a Nilan Air Vent System (Danish Brand) using a Wemos D1
+  Mini (or other ESP8266-based board) and report the values to an MQTT
+  broker. Then use it for your home-automation system like Home Assistant.
+ 
+  External dependencies. Install using the Arduino library manager:
+ 
+     "Arduino JSON V6 by Benoît Blanchon https://github.com/bblanchon/ArduinoJson - IMPORTANT - Use latest V.6 !!! This code won´t compile with V.5
+     "ModbusMaster by Doc Walker https://github.com/4-20ma/ModbusMaster
+     "PubSubClient" by Nick O'Leary https://github.com/knolleary/pubsubclient
+     
+  Project inspired by https://github.com/DanGunvald/NilanModbus
+  Join this Danish Facebook Page for inspiration :) https://www.facebook.com/groups/667765647316443/
+*/
+ 
 #include <ArduinoJson.h>
 #include <ESP8266WiFi.h>
 #include <ArduinoOTA.h>
@@ -13,30 +36,31 @@
  
 #define HOST "NilanGW-%s" // Change this to whatever you like.
 #define MAXREGSIZE 26
-#define SENDINTERVAL 30000 // normally set to 180000 milliseconds = 3 minutes. Define as you like
+#define SENDINTERVAL 3000 // normally set to 180000 milliseconds = 3 minutes. Define as you like
 #define VENTSET 1003
 #define RUNSET 1001
 #define MODESET 1002
 #define TEMPSET 1004
+#define PROGRAMSET 500
  
 #if SERIAL == SERIAL_SOFTWARE
 SoftwareSerial SSerial(SERIAL_SOFTWARE_RX, SERIAL_SOFTWARE_TX); // RX, TX
 #endif
  
-const char* ssid = WIFISSID;
-const char* password = WIFIPASSWORD;
+const char *ssid = WIFISSID;
+const char *password = WIFIPASSWORD;
 char chipid[12];
-const char* mqttserver = MQTTSERVER;
-const char* mqttusername = MQTTUSERNAME;
-const char* mqttpassword = MQTTPASSWORD;
+const char *mqttserver = MQTTSERVER;
+const char *mqttusername = MQTTUSERNAME;
+const char *mqttpassword = MQTTPASSWORD;
 WiFiServer server(80);
 WiFiClient client;
 PubSubClient mqttclient(client);
 static long lastMsg = -SENDINTERVAL;
 static int16_t rsbuffer[MAXREGSIZE];
 ModbusMaster node;
-
-int RELAY1 = D5; // Relay PIN - could be different pin - this is the pin used on af wemos relay shield for d1 mini
+char *usersetTopic1 = "ventilation/relay/userset";  // bruger valg
+char *usersetTopic2 = "ventilation/relay/userset2"; // bruger valg
  
 String req[4]; //operation, group, address, value
 enum reqtypes
@@ -49,6 +73,7 @@ enum reqtypes
   reqairtemp,
   reqairflow,
   reqairheat,
+  reqprogram,
   requser,
   requser2,
   reqinfo,
@@ -61,10 +86,10 @@ enum reqtypes
   reqmax
 };
  
-String groups[] = {"temp", "alarm", "time", "control", "speed", "airtemp", "airflow", "airheat", "user", "user2", "info", "inputairtemp", "app", "output", "display1", "display2", "display"};
-byte regsizes[] = {23, 10, 6, 8, 2, 6, 2, 0, 6, 6, 14, 7, 4, 26, 4, 4, 1};
-int regaddresses[] = {200, 400, 300, 1000, 200, 1200, 1100, 0, 600, 610, 100, 1200, 0, 100, 2002, 2007, 3000};
-byte regtypes[] = {8, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 2, 1, 4, 4, 8};
+String groups[] = {"temp", "alarm", "time", "control", "speed", "airtemp", "airflow", "airheat", "program", "user", "user2", "info", "inputairtemp", "app", "output", "display1", "display2", "display"};
+byte regsizes[] = {23, 10, 6, 8, 2, 6, 2, 0, 1, 6, 6, 14, 7, 4, 26, 4, 4, 1};
+int regaddresses[] = {200, 400, 300, 1000, 200, 1200, 1100, 0, 500, 600, 610, 100, 1200, 0, 100, 2002, 2007, 3000};
+byte regtypes[] = {8, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 2, 1, 4, 4, 8};
 char *regnames[][MAXREGSIZE] = {
     //temp
     {"T0_Controller", NULL, NULL, "T3_Exhaust", "T4_Outlet", NULL, NULL, "T7_Inlet", "T8_Outdoor", NULL, NULL, NULL, NULL, NULL, NULL, "T15_Room", NULL, NULL, NULL, NULL, NULL, "RH", NULL},
@@ -82,6 +107,8 @@ char *regnames[][MAXREGSIZE] = {
     {"AirExchMode", "CoolVent"},
     //airheat
     {},
+    //program
+    {"Program"},
     //program.user
     {"UserFuncAct", "UserFuncSet", "UserTimeSet", "UserVentSet", "UserTempSet", "UserOffsSet"},
     //program.user2
@@ -110,7 +137,7 @@ char *getName(reqtypes type, int address)
   return NULL;
 }
  
-JsonObject HandleRequest(JsonDocument& doc)
+JsonObject HandleRequest(JsonDocument &doc)
 {
   JsonObject root = doc.to<JsonObject>();
   reqtypes r = reqmax;
@@ -137,6 +164,7 @@ JsonObject HandleRequest(JsonDocument& doc)
     result = ReadModbus(address, nums, rsbuffer, type & 1);
     if (result == 0)
     {
+      root["status"] = "Modbus connection OK";
       for (int i = 0; i < nums; i++)
       {
         char *name = getName(r, i);
@@ -160,6 +188,10 @@ JsonObject HandleRequest(JsonDocument& doc)
         }
       }
     }
+    else
+    {
+      root["status"] = "Modbus connection failed";
+    }
     root["requestaddress"] = address;
     root["requestnum"] = nums;
   }
@@ -181,36 +213,27 @@ JsonObject HandleRequest(JsonDocument& doc)
   }
   root["operation"] = req[0];
   root["group"] = req[1];
+  return root;
 }
  
 void setup()
 {
-  if(USE_WIFI_LED) pinMode(WIFI_LED, OUTPUT);
+  pinMode(RELAY1_PIN, OUTPUT); // bruger valg
+  digitalWrite(RELAY1_PIN, LOW);
+  pinMode(RELAY2_PIN, OUTPUT); // bruger valg
+  digitalWrite(RELAY2_PIN, LOW);
   char host[64];
   sprintf(chipid, "%08X", ESP.getChipId());
   sprintf(host, HOST, chipid);
   delay(500);
-  if(CUSTOM_HOSTNAME)
-  {
-    WiFi.hostname(CUSTOM_HOSTNAME);
-    ArduinoOTA.setHostname(CUSTOM_HOSTNAME);
-  } else
-  {
-    WiFi.hostname(host);
-    ArduinoOTA.setHostname(host);
-  }
+  WiFi.hostname(host);
+  ArduinoOTA.setHostname(host);
   WiFi.mode(WIFI_STA);
   WiFi.begin(ssid, password);
- 
   while (WiFi.waitForConnectResult() != WL_CONNECTED)
   {
-    if(USE_WIFI_LED) digitalWrite(WIFI_LED, !digitalRead(WIFI_LED));
     delay(5000);
     ESP.restart();
-  }
-  if (WiFi.status() == WL_CONNECTED && USE_WIFI_LED)
-  {
-    digitalWrite(WIFI_LED, 0);
   }
   ArduinoOTA.onStart([]() {
   });
@@ -223,17 +246,17 @@ void setup()
   ArduinoOTA.begin();
   server.begin();
  
-  #if SERIAL == SERIAL_SOFTWARE
-    #warning Compiling for software serial
-    SSerial.begin(19200); // SERIAL_8E1
-    node.begin(30, SSerial);
-  #elif SERIAL == SERIAL_HARDWARE
-    #warning Compiling for hardware serial
-    Serial.begin(19200, SERIAL_8E1);
-    node.begin(30, Serial);
-  #else
-    #error hardware og serial serial port?
-  #endif
+#if SERIAL == SERIAL_SOFTWARE
+#warning Compiling for software serial
+  SSerial.begin(19200); // SERIAL_8E1
+  node.begin(30, SSerial);
+#elif SERIAL == SERIAL_HARDWARE
+#warning Compiling for hardware serial
+  Serial.begin(19200, SERIAL_8E1);
+  node.begin(30, Serial);
+#else
+#error hardware og serial serial port?
+#endif
  
   mqttclient.setServer(mqttserver, 1883);
   mqttclient.setCallback(mqttcallback);
@@ -265,6 +288,32 @@ void mqttcallback(char *topic, byte *payload, unsigned int length)
       WriteModbus(RUNSET, run);
     }
   }
+  if (strcmp(topic, "ventilation/relay/userset") == 0) // bruger valg
+  {
+    if (payload[0] == '1')
+    {
+      digitalWrite(RELAY1_PIN, HIGH);
+      mqttclient.publish("ventilation/relay/userset", "on");
+    }
+    else if (payload[0] == '0')
+    {
+      digitalWrite(RELAY1_PIN, LOW);
+      mqttclient.publish("ventilation/relay/userset", "off");
+    }
+  }
+  if (strcmp(topic, "ventilation/relay/userset2") == 0) // bruger valg
+  {
+    if (payload[0] == '1')
+    {
+      digitalWrite(RELAY2_PIN, HIGH);
+      mqttclient.publish("ventilation/relay/userset2", "on");
+    }
+    else if (payload[0] == '0')
+    {
+      digitalWrite(RELAY2_PIN, LOW);
+      mqttclient.publish("ventilation/relay/userset2", "off");
+    }
+  }
   if (strcmp(topic, "ventilation/tempset") == 0)
   {
     if (length == 4 && payload[0] >= '0' && payload[0] <= '2')
@@ -277,16 +326,12 @@ void mqttcallback(char *topic, byte *payload, unsigned int length)
       WriteModbus(TEMPSET, str.toInt());
     }
   }
-    if (strcmp(topic, "ventilation/custom/relay") == 0)
+  if (strcmp(topic, "ventilation/programset") == 0)
+  {
+    if (length == 1 && payload[0] >= '0' && payload[0] <= '4')
     {
-     if (length == 4 && payload[0] >= '0' && payload[0] <= '2')
-    {
-      String str;
-      for (int i = 0; i < length; i++)
-      {
-        str += (char)payload[i];
-      }
-      digitalWrite(RELAY1, str.toInt());
+      int16_t program = payload[0] - '0';
+      WriteModbus(PROGRAMSET, program);
     }
   }
   lastMsg = -SENDINTERVAL;
@@ -328,13 +373,13 @@ bool readRequest(WiFiClient &client)
   return false;
 }
  
-void writeResponse(WiFiClient& client, const JsonDocument& doc)  
+void writeResponse(WiFiClient &client, const JsonDocument &doc)
 {
   client.println("HTTP/1.1 200 OK");
   client.println("Content-Type: application/json");
   client.println("Connection: close");
   client.println();
-  serializeJsonPretty(doc,client);
+  serializeJsonPretty(doc, client);
 }
  
 char ReadModbus(uint16_t addr, uint8_t sizer, int16_t *vals, int type)
@@ -378,6 +423,9 @@ void mqttreconnect()
       mqttclient.subscribe("ventilation/modeset");
       mqttclient.subscribe("ventilation/runset");
       mqttclient.subscribe("ventilation/tempset");
+      mqttclient.subscribe("ventilation/programset");
+      mqttclient.subscribe("ventilation/relay/userset");  // bruger valg
+      mqttclient.subscribe("ventilation/relay/userset2"); // bruger valg
     }
     else
     {
@@ -401,7 +449,7 @@ void loop()
     bool success = readRequest(client);
     if (success)
     {
-      StaticJsonDocument<500> doc;
+      StaticJsonDocument<1000> doc;
       HandleRequest(doc);
  
       writeResponse(client, doc);
@@ -420,13 +468,14 @@ void loop()
     long now = millis();
     if (now - lastMsg > SENDINTERVAL)
     {
-      reqtypes rr[] = {reqtemp, reqcontrol, reqoutput, reqspeed, reqalarm, reqinputairtemp, requser, reqdisplay}; // put another register in this line to subscribe
-      for (int i = 0; i < (sizeof(rr)/sizeof(rr[0])); i++)
+      reqtypes rr[] = {reqtemp, reqcontrol, reqtime, reqoutput, reqspeed, reqalarm, reqinputairtemp, reqprogram, requser, reqdisplay, reqinfo}; // put another register in this line to subscribe
+      for (int i = 0; i < (sizeof(rr) / sizeof(rr[0])); i++)
       {
         reqtypes r = rr[i];
         char result = ReadModbus(regaddresses[r], regsizes[r], rsbuffer, regtypes[r] & 1);
         if (result == 0)
         {
+          mqttclient.publish("ventilation/error/modbus/", "0"); //no error when connecting through modbus
           for (int i = 0; i < regsizes[r]; i++)
           {
             char *name = getName(r, i);
@@ -438,6 +487,10 @@ void loop()
               {
               case reqcontrol:
                 mqname = "ventilation/control/"; // Subscribe to the "control" register
+                itoa((rsbuffer[i]), numstr, 10);
+                break;
+              case reqtime:
+                mqname = "ventilation/time/"; // Subscribe to the "output" register
                 itoa((rsbuffer[i]), numstr, 10);
                 break;
               case reqoutput:
@@ -460,14 +513,25 @@ void loop()
                 mqname = "ventilation/inputairtemp/"; // Subscribe to the "inputairtemp" register
                 itoa((rsbuffer[i]), numstr, 10);
                 break;
+              case reqprogram:
+                mqname = "ventilation/weekprogram/"; // Subscribe to the "week program" register
+                itoa((rsbuffer[i]), numstr, 10);
+                break;
               case requser:
                 mqname = "ventilation/user/"; // Subscribe to the "user" register
                 itoa((rsbuffer[i]), numstr, 10);
-                break;          
+                break;
+              case reqinfo:
+                mqname = "ventilation/info/"; // Subscribe to the "info" register
+                itoa((rsbuffer[i]), numstr, 10);
+                break;
               case reqtemp:
-                if (strncmp("RH", name, 2) == 0) {
+                if (strncmp("RH", name, 2) == 0)
+                {
                   mqname = "ventilation/moist/"; // Subscribe to moisture-level
-                } else {
+                }
+                else
+                {
                   mqname = "ventilation/temp/"; // Subscribe to "temp" register
                 }
                 dtostrf((rsbuffer[i] / 100.0), 5, 2, numstr);
@@ -478,45 +542,76 @@ void loop()
             }
           }
         }
-      }
- 
-      // Handle text fields
-      reqtypes rr2[] = {reqdisplay1, reqdisplay2}; // put another register in this line to subscribe
-      for (int i = 0; i < (sizeof(rr2)/sizeof(rr2[0])); i++) // change value "5" to how many registers you want to subscribe to
-      {
-        reqtypes r = rr2[i];
- 
-        char result = ReadModbus(regaddresses[r], regsizes[r], rsbuffer, regtypes[r] & 1);
-        if (result == 0)
+        else
         {
-          String text = "";
-          String mqname = "ventilation/text/";
- 
-          for (int i = 0; i < regsizes[r]; i++)
-          {
-              char *name = getName(r, i);
- 
-              if ((rsbuffer[i] & 0x00ff) == 0xDF) {
-                text += (char)0x20; // replace degree sign with space
-              } else {
-                text += (char)(rsbuffer[i] & 0x00ff);
-              }
-              if ((rsbuffer[i] >> 8) == 0xDF) {
-                text += (char)0x20; // replace degree sign with space
-              } else {
-                text += (char)(rsbuffer[i] >> 8);
-              }
-              mqname += (char *)name;
-          }
-          mqttclient.publish(mqname.c_str(), text.c_str());
+          mqttclient.publish("ventilation/error/modbus/", "1"); //error when connecting through modbus
         }
       }
-      
-      // Custom relay
-      String mqname = "ventilation/custom/relay1";
-      mqttclient.publish(mqname.c_str(), String(digitalRead(RELAY1)).c_str());
-
-      lastMsg = now;
+ 
+      // Tell the current state of RELAY1
+      if (now - lastMsg > SENDINTERVAL)
+      {
+        if (digitalRead(RELAY1_PIN))
+        {
+          mqttclient.publish("ventilation/relay/relay1/state", "on");
+        }
+        else
+        {
+          mqttclient.publish("ventilation/relay/relay1/state", "off");
+        }
+ 
+        // Tell the current state of RELAY2
+        if (now - lastMsg > SENDINTERVAL)
+        {
+          if (digitalRead(RELAY2_PIN))
+          {
+            mqttclient.publish("ventilation/relay/relay2/state", "on");
+          }
+          else
+          {
+            mqttclient.publish("ventilation/relay/relay2/state", "off");
+          }
+ 
+          // Handle text fields
+          reqtypes rr2[] = {reqdisplay1, reqdisplay2}; // put another register in this line to subscribe
+          for (int i = 0; i < (sizeof(rr2) / sizeof(rr2[0])); i++)
+          {
+            reqtypes r = rr2[i];
+ 
+            char result = ReadModbus(regaddresses[r], regsizes[r], rsbuffer, regtypes[r] & 1);
+            if (result == 0)
+            {
+              String text = "";
+              String mqname = "ventilation/text/";
+ 
+              for (int i = 0; i < regsizes[r]; i++)
+              {
+                char *name = getName(r, i);
+ 
+                if ((rsbuffer[i] & 0x00ff) == 0xDF)
+                {
+                  text += (char)0x20; // replace degree sign with space
+                }
+                else
+                {
+                  text += (char)(rsbuffer[i] & 0x00ff);
+                }
+                if ((rsbuffer[i] >> 8) == 0xDF)
+                {
+                  text += (char)0x20; // replace degree sign with space
+                }
+                else
+                {
+                  text += (char)(rsbuffer[i] >> 8);
+                }
+                mqname += (char *)name;
+              }
+              mqttclient.publish(mqname.c_str(), text.c_str());
+            }
+          }
+          lastMsg = now;
+        }
+      }
     }
   }
 }
