a8296dfc18518443757ec7865f320a4e47fa596d
zinen <zinen@users.noreply.github.com>
2022-05-30 17:41:28 +0200

    Configuration can be in seperate file now

--- src/configuration.h
+++ src/configuration.h
@@ -5,21 +5,30 @@
  */
 
 // Change config to verify that project has been configured
+#ifndef CONFIGURED 
 #define CONFIGURED 0 // Change this to "1" when you completed your config and begin upload
+#endif
 
 // WIFI settings
+#ifndef WIFI_SSID // If already defined skip defining it again
 #define WIFI_SSID     "XX" // Put in your SSID 
 #define WIFI_PASSWORD "XX" // Put in your SSID Password 
+#endif
 
 // LED settings
 #define WIFI_LED LED_BUILTIN // Blue led on NodeMCU
 #define USE_WIFI_LED true // if 'true', the blue led in a NodeMCU will blink during connection,
                            // and glow solid once connected
 // MQTT settings
+#ifndef MQTT_SERVER // If already defined skip defining it again
 #define MQTT_SERVER   "XX" // Put in the IP addresses of your MQTT broker
+#endif
+#ifndef MQTT_USERNAME // If already defined skip defining it again
 #define MQTT_USERNAME NULL // Username for the MQTT broker (NULL if no username is required)
 #define MQTT_PASSWORD NULL // Password for the MQTT broker (NULL if no password is required)
+#endif
 #define MQTT_SEND_INTERVAL 600000 // normally set to 180000 milliseconds = 3 minutes. Define as you like
+
 
 // Serial port
 #define SERIAL_CHOICE SERIAL_HARDWARE // SERIAL_SOFTWARE or SERIAL_HARDWARE
